5b4b7c49c8444bb1c5722e137963c3210f5e5adf
emirramon <emirramoon@gmail.com>
2019-10-10 18:19:36 +0700

    first commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	public function index(){
+		$this->template->load('template_utama','Home/index');
+	}
+}
